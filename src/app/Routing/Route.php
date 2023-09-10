eb95153aef5f6f8b8d400a964ea090a84147bc0c
E1win <87700553+E1win@users.noreply.github.com>
2023-09-10 12:28:01 +0200

    Route class setup

---
+++ src/app/Routing/Route.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Routing;
+
+class Route
+{
+  /**
+   * URI pattern the route responds to
+   */
+  protected string $uri;
+
+  /**
+   * HTTP methods the route responds to (GET, POST, etc.)
+   */
+  protected array $methods;
+
+  /**
+   * Route action array
+   */
+  protected array $action;
+
+  /**
+   * Controller instance
+   */
+  protected mixed $controller;
+
+  public function __construct($methods, string $uri, $action)
+  {
+    $this->uri = $uri;
+    // TODO: Check if method(s) are valid
+    $this->methods = (array) $methods;
+    // TODO: Check if action is valid
+    $this->action = $action;
+  }
+
+  /**
+   * Run the route action and return the response
+   */
+  public function run(): mixed
+  {
+    try {
+      return $this->runController();
+    } catch (\Exception $e) {
+      // Return exception response
+    }
+  }
+
+  /**
+   * Run the route controller action and return the response
+   */
+  protected function runController()
+  {
+
+  }
+}
