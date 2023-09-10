ed16ad66c5f8a78150c9b713ca6e4f723d23108f
E1win <87700553+E1win@users.noreply.github.com>
2023-09-10 14:05:04 +0200

    created bootstrap app.php and index.php basic structure

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,23 @@
 <?php
 
+/**
+ * ADDING COMPOSER AUTOLOADER
+ * 
+ * So we no longer have to manually load our classes.
+ */
+
+// require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * RUN APPLICATION
+ * 
+ * Let application handle incoming request.
+ * Then, send appropiate response back to client.
+ */
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+// Add application here
+
+// send response
+
 echo 'Hello World!';
