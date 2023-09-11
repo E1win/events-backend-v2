240687da9483cbee005c996de9565e91c2e554e2
E1win <87700553+E1win@users.noreply.github.com>
2023-09-11 11:17:14 +0200

    filled MiddlewareStack class

--- src/framework/Middleware/MiddlewareStack.php
+++ src/framework/Middleware/MiddlewareStack.php
@@ -1,7 +1,43 @@
 <?php
 namespace Framework\Middleware;
 
+use Psr\Http\Server\MiddlewareInterface;
+
 class MiddlewareStack
 {
-  // . . .
+  /**
+   * Array containing all middleware in stack
+   * 
+   * @var MiddlewareInterface[]
+   */
+  protected array $stack = [];
+
+  public function __construct(MiddlewareInterface ...$middlewares)
+  {
+    $this->stack = (array) $middlewares ?? [];
+  }
+
+  public function getStack(): array
+  {
+    return $this->stack;
+  }
+
+  public function shift(): MiddlewareInterface
+  {
+    return array_shift($this->stack);
+  }
+
+  public function prepend(MiddlewareInterface $middleware): self
+  {
+    array_unshift($this->stack, $middleware);
+
+    return $this;
+  }
+
+  public function append(MiddlewareInterface $middleware): self
+  {
+    $this->stack[] = $middleware;
+
+    return $this;
+  }
 }
