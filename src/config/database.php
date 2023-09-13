8f377169ceca73906b7de72a8e277e9d33a4e89c
E1win <87700553+E1win@users.noreply.github.com>
2023-09-13 10:43:35 +0200

    setup database config file

---
+++ src/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'driver' => 'mysql',
+  'url' => 'GETFROMENV',
+  'port' => 'GETFROMENV',
+  'database_name' => 'GETFROMENV',
+  'user' => 'GETFROMENV',
+  'password' => 'GETFROMENV',
+  'options' => '',
+];
