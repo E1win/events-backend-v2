04d5f1628016001a66174ede38352884df77bc0a
E1win <87700553+E1win@users.noreply.github.com>
2023-09-18 12:19:00 +0200

    made container static member variable in App

--- framework/Application/App.php
+++ framework/Application/App.php
@@ -2,20 +2,29 @@
 namespace Framework\Application;
 
 use Framework\Container\Container;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-// Probably have container as a static instance
-
 class App
 {
+  private static ContainerInterface $container;
+
   public function __construct(Container $container)
   {
-    
+    $this->container = $container;
+
+    // add middlewares, etc.
   }
 
   public function handle(ServerRequestInterface $request): ResponseInterface
   {
-    // . . .
+    // dispatcher
+    // routes
+  }
+
+  public function getContainer(): ContainerInterface
+  {
+    return $this->container;
   }
 }
