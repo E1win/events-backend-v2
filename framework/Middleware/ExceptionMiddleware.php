86de0480518660647828dbcbf0fd816b024dd4c0
E1win <87700553+E1win@users.noreply.github.com>
2023-09-27 12:42:10 +0200

    ExceptionMiddleware setup

--- framework/Middleware/ExceptionMiddleware.php
+++ framework/Middleware/ExceptionMiddleware.php
@@ -1,26 +1,53 @@
 <?php
 namespace Framework\Middleware;
 
+use Framework\Message\Contract\HtmlResponseFactoryInterface;
 use Framework\Message\Contract\JsonResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
 
 class ExceptionMiddleware implements MiddlewareInterface
 {
+  private const CONTENT_TYPE_CONVERSION = [
+    'application/json' => 'application/problem+json',
+    'application/xml' => 'application/problem+xml',
+  ];
+
   public function __construct(
     private JsonResponseFactoryInterface $jsonResponseFactory,
+    private HtmlResponseFactoryInterface $htmlResponseFactory,
   ) { }
 
   public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
   {
-    return $handler->handle($request);
     try {
-    } catch (\Throwable $e) {
-      throw new $e;
-      // Return exception as response
-      // formatting here
+      return $handler->handle($request);
+    } catch (\Throwable $error) {
+      // todo: Get better way to add debug information
+      if ($_ENV['APP_DEBUG'] == "true") {
+        throw new $error;
+      }
+
+      return $this->generateResponse($request, $error);
     }
+  }
+
+  private function generateResponse(ServerRequestInterface $request, Throwable $error): ResponseInterface
+  {
+    $response = $this->jsonResponseFactory->createJsonResponse($error->getMessage(), $error->getCode());
+    
+    $accept = $request->getHeaderLine('Accept');
+
+    if (!array_key_exists($accept, self::CONTENT_TYPE_CONVERSION)) {
+      return $response;
+    }
+
+    return $response->withAddedHeader(
+      'Content-Type',
+      self::CONTENT_TYPE_CONVERSION[$accept] . '; charset=' . $request->getHeaderLine('Accept-Charset')
+    );
   }
 }
