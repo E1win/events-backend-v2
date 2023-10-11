6d160d2d0a10332b3d770c7b96954daad2ce1a73
E1win <87700553+E1win@users.noreply.github.com>
2023-10-11 14:26:31 +0200

    updated User Entity

--- framework/Auth/Model/Entity/User.php
+++ framework/Auth/Model/Entity/User.php
@@ -6,11 +6,20 @@ use Framework\Model\Entity\Entity;
 class User extends Entity
 {
   protected ?int $id;
+
   protected ?string $email = null;
   protected ?string $password = null;
 
+  protected ?string $firstName = null;
+  protected ?string $prefix = null;
+  protected ?string $lastName = null;
+
   protected ?string $sessionUuid = null;
   protected ?int $expiresOn = null;
+
+  protected ?bool $activeAccount = null;
+  protected ?int $roleId = null;
+
 
   public function __construct(?int $id = null)
   {
@@ -51,6 +60,49 @@ class User extends Entity
   {
     return true;
   }
+  
+  public function setFirstName(string $firstName)
+  {
+    $this->firstName = $firstName;
+  }
+
+  public function getFirstName()
+  {
+    return $this->firstName;
+  }
+
+  public function setPrefix(string $prefix)
+  {
+    $this->prefix = $prefix;
+  }
+
+  public function getPrefix()
+  {
+    return $this->prefix;
+  }
+
+  public function setLastName(string $lastName)
+  {
+    $this->lastName = $lastName;
+  }
+  
+  public function getLastName()
+  {
+    return $this->lastName;
+  }
+
+  public function getFullName()
+  {
+    if ($this->firstName == null || $this->lastName == null) {
+      return null;
+    }
+
+    if (isset($this->prefix)) {
+      return $this->firstName . ' ' . $this->prefix . ' ' . $this->lastName;
+    }
+
+    return $this->firstName . ' ' . $this->lastName;
+  }
 
   public function setSessionUuid(string $sessionUuid)
   {
@@ -76,4 +128,25 @@ class User extends Entity
   {
     return time() > $this->expiresOn;
   }
+
+  public function setActiveAccount(bool $activeAccount)
+  {
+    $this->activeAccount = $activeAccount;
+  }
+
+  public function getActiveAccount()
+  {
+    return $this->activeAccount;
+  }
+
+  public function setRoleId(string $roleId)
+  {
+    $this->roleId = $roleId;
+  }
+
+  public function getRoleId()
+  {
+    return $this->roleId;
+  }
+
 }
