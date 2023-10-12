2a340b4ff36643c2b5a49de2e4792f076126a957
E1win <87700553+E1win@users.noreply.github.com>
2023-10-12 11:50:09 +0200

    default timezone zet

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -26,8 +26,7 @@ $_ENV['DB_USER'] = "root";
 $_ENV['DB_PASS'] = "root";
 
 
-// https://prototype.php.net/manual/en/function.date-default-timezone-set.php
-// Maybe use later for setting timezone
+date_default_timezone_set('Europe/Amsterdam');
 
 // Load helper functions
 require __DIR__ . '/functions.php';
