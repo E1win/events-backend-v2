8aa5a7af83fba22c24b231a9851afa57bb6c8a25
E1win <87700553+E1win@users.noreply.github.com>
2023-10-22 15:45:24 +0200

    only allow Admin accounts on dashboard

--- config/routes/web.php
+++ config/routes/web.php
@@ -5,7 +5,7 @@ use Framework\Routing\Router;
 use App\Http\Controller\Web\AuthController;
 use App\Http\Controller\Web\EventController;
 use App\Http\Controller\Web\ViewController;
-
+use Framework\Auth\Middleware\AdminAuthMiddleware;
 use Framework\Auth\Middleware\AuthMiddleware;
 use Framework\Auth\Middleware\LoginRedirectMiddleware;
 use Framework\Auth\Middleware\UserInRequestMiddleware;
@@ -17,17 +17,19 @@ $router->addMiddlewares([
 ]);
 
 
-$router->get('/events', [EventController::class, 'index']);
-$router->post('/events', [EventController::class, 'store']);
-$router->get('/events/{id:number}', [EventController::class, 'show']);
 
 $router->get('/login', [AuthController::class, 'login']);
 
 $router->group('', function(Router $router) {
   $router->get('/', [ViewController::class, 'home']);
+  
+  $router->get('/events', [EventController::class, 'index']);
+  $router->post('/events', [EventController::class, 'store']);
+  $router->get('/events/{id:number}', [EventController::class, 'show']);
 })->addMiddlewares([
   UserInRequestMiddleware::class,
-  AuthMiddleware::class
+  AuthMiddleware::class,
+  AdminAuthMiddleware::class
 ]);
 
 return $router;
