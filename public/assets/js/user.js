8f3ca48812f800cef41d293537c0c6f762406e0f
E1win <87700553+E1win@users.noreply.github.com>
2023-11-22 12:15:54 +0100

    show participants on event detailpage

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -4,6 +4,13 @@ function userData() {
     setCurrentUser(user) {
       this.currentUser = user;
     },
+    formatNameOfUser(user) {
+      if (user.prefix != null) {
+        return `${user.firstName} ${user.prefix} ${user.lastName}`;
+      }
+
+      return `${user.firstName} ${user.lastName}`;
+    }
   }
 }
 /**
