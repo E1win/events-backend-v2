03dc578553a7cd2149612673fb7a441a3eed3b9a
E1win <87700553+E1win@users.noreply.github.com>
2023-11-23 11:17:17 +0100

    get db from .env again

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -6,25 +6,8 @@ define('ROOT_PATH', __DIR__ . "/../");
 require __DIR__ . '/../vendor/autoload.php';
 
 // Load environment variables from .env into $_ENV
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-// $dotenv->load();
-
-// APP_DEBUG=true
-
-// DB_HOST=db
-// DB_PORT=3306
-// DB_NAME=events
-// DB_USER=root
-// DB_PASS=root
-
-$_ENV['APP_DEBUG'] = true;
-
-$_ENV['DB_HOST'] = 'db';
-$_ENV['DB_PORT'] = 3306;
-$_ENV['DB_NAME'] = "events";
-$_ENV['DB_USER'] = "root";
-$_ENV['DB_PASS'] = "root";
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 date_default_timezone_set('Europe/Amsterdam');
 
